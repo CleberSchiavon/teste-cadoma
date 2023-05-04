ff5f491dac5f8ee5fe2ee386c8c68a7f5056e2b4
Cleber Henrique <cleberschiavon@outlook.com>
2023-05-04 01:50:09 -0300

    feat: creating DashboardWidget component

---
+++ src/components/dashboard/DashboardWidget/index.tsx
@@ -0,0 +1,62 @@
+import { Card, SvgIconTypeMap, Theme, Typography } from '@mui/material';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+
+// utils
+import { StyledIcon } from './styled';
+
+interface IDashboardWidget {
+  title: string;
+  total: number;
+  sx?: object;
+  Icon: OverridableComponent<SvgIconTypeMap<'svg'>> & {
+    muiName: string;
+  };
+  color?: string;
+}
+
+export default function DashboardWidget({
+  title,
+  total,
+  Icon,
+  color = 'primary',
+  sx,
+  ...other
+}: IDashboardWidget) {
+  return (
+    <Card
+      sx={{
+        py: 5,
+        boxShadow: 'black 0px 16px 23px -16px',
+        textAlign: 'center',
+        // @ts-expect-error Espero erro aqui pois o typescript não tem como adivinhar qual vai ser a cor
+        color: (theme: Theme) => theme.palette[color].dark,
+        // @ts-expect-error Espero erro aqui pois o typescript não tem como adivinhar qual vai ser a cor
+        bgcolor: (theme: Theme) => theme.palette[color].dark,
+        backgroundImage: (theme: Theme) =>
+          // @ts-expect-error Espero erro aqui pois o typescript não tem como adivinhar qual vai ser a cor
+          `linear-gradient(315deg, ${theme.palette[color].light} 0%, ${theme.palette[color].dark} 74%);
+          `,
+        ...sx,
+      }}
+      {...other}
+    >
+      <StyledIcon>
+        <Icon />
+      </StyledIcon>
+
+      {title === 'Rendimento operacional' ? (
+        <Typography variant='h3' color='white'>
+          {total}%
+        </Typography>
+      ) : (
+        <Typography variant='h3' color='white'>
+          {total}
+        </Typography>
+      )}
+
+      <Typography variant='subtitle2' color='white' sx={{ opacity: 0.72 }}>
+        {title}
+      </Typography>
+    </Card>
+  );
+}
