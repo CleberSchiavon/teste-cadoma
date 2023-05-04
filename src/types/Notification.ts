b8322cf5e06efae9806c6403b7e5753f88c388ec
Cleber Henrique <cleberschiavon@outlook.com>
2023-05-04 01:51:52 -0300

    feat: creating home types

---
+++ src/types/Notification.ts
@@ -0,0 +1,18 @@
+export type Notification = {
+  id: number;
+  type: string;
+  image: string;
+  createdAt: number;
+  title: string;
+  isUnread: boolean;
+};
+
+export interface IDashboardNotification {
+  title: string;
+  subheader?: string;
+  list: Notification[];
+}
+
+export interface NotificationItem {
+  notification: Notification;
+}
